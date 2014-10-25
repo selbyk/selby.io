173d8a0052510a4eb8c00d859c6762e7d47877ab
Selby Kendrick <selby.kendrick@gmail.com>
2014-10-24 21:58:05 -0500

    deployable

--- selby.io.js
+++ selby.io.js
@@ -4,8 +4,23 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
+var sys = require('sys');
+var exec = require('child_process').exec;
 server.listen(port, function () {
 console.log('Server listening at port %d', port);
+});
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.get('/deploy', function(req, res) {
+    var body = 'Deployed';
+    function puts(error, stdout, stderr) { body = body + stdout; }
+    exec("git pull", puts);
+    exec("forever restart selby.io.js", puts);
+    res.send(body);
 });
 // Routing
 app.use(express.static(__dirname + '/dist'));
