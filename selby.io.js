cbb5c10f1c8ad65ead8eae7405b309be8d4f8200
Selby Kendrick <selby.kendrick@gmail.com>
2014-10-27 12:57:13 -0500

    socket.io, production

--- selby.io.js
+++ selby.io.js
@@ -15,6 +15,21 @@ var connection = mysql.createConnection({
   database: 'freenode'
 });
 
+io.configure('production', function(){
+        console.log(doodlr + " set config for production");
+        io.enable('browser client minification');  // send minified client
+        io.enable('browser client etag');          // apply etag caching logic based on version number
+        io.enable('browser client gzip');          // gzip the file
+        io.set('log level', 1);                    // reduce logging
+        io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+            'websocket'
+          , 'flashsocket'
+          , 'htmlfile'
+          , 'xhr-polling'
+          , 'jsonp-polling'
+        ]);
+      });
+
 //connection.connect();
 
 var sql = "INSERT INTO `message` (`time`, `to`, `from`, `message`) VALUES ?";
