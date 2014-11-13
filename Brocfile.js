4b790402ae9f8fae98055bd9adf6f7994b22c943
Selby Kendrick <selby.kendrick@gmail.com>
2014-11-13 01:21:08 -0600

    showchase?

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,6 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/socket.io-client/socket.io.js');
+app.import('bower_components/d3/d3.min.js');
 module.exports = app.toTree();
