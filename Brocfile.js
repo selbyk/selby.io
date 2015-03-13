40a65cfc7ebc40b5182aad45a6dca10c000d4542
selby <selby.kendrick@gmail.com>
2015-03-13 10:03:18 -0500

    hmmm, lets see how this fares

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var uglifyJavaScript = require('broccoli-uglify-js');
 
 var app = new EmberApp({
   vendorFiles: {
@@ -61,4 +62,31 @@ app.import({
 
 app.import("bower_components/bootstrap-less/js/dropdown.js");
 
-module.exports = app.toTree();
+var tree = app.toTree();
+
+tree = uglifyJavaScript(tree, {
+  mangle: true,
+  compress: {
+    sequences     : true,  // join consecutive statemets with the “comma operator”
+    properties    : true,  // optimize property access: a["foo"] → a.foo
+    dead_code     : true,  // discard unreachable code
+    drop_debugger : true,  // discard “debugger” statements
+    unsafe        : false, // some unsafe optimizations (see below)
+    conditionals  : true,  // optimize if-s and conditional expressions
+    comparisons   : true,  // optimize comparisons
+    evaluate      : true,  // evaluate constant expressions
+    booleans      : true,  // optimize boolean expressions
+    loops         : true,  // optimize loops
+    unused        : true,  // drop unused variables/functions
+    hoist_funs    : true,  // hoist function declarations
+    hoist_vars    : false, // hoist variable declarations
+    if_return     : true,  // optimize if-s followed by return/continue
+    join_vars     : true,  // join var declarations
+    cascade       : true,  // try to cascade `right` into `left` in sequences
+    side_effects  : true,  // drop side-effect-free statements
+    warnings      : true,  // warn about potentially dangerous optimizations/code
+    global_defs   : {}     // global definitions
+  }
+});
+
+module.exports = tree;
