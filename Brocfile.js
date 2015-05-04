24c458c3723d6c757058f990c2f8963a931c1d01
selby <selby.kendrick@gmail.com>
2015-05-04 17:30:03 -0500

    code highlighting

--- Brocfile.js
+++ Brocfile.js
@@ -1,12 +1,12 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
-    isProduction = EmberApp.env() === 'production',
-    uglifyJavaScript = require('broccoli-uglify-js');
+  isProduction = EmberApp.env() === 'production',
+  uglifyJavaScript = require('broccoli-uglify-js');
 
 var app = new EmberApp({
   vendorFiles: {
-   'handlebars.js': null
+    'handlebars.js': null
   }
 });
 
@@ -53,41 +53,43 @@ app.import("bower_components/ionicons/fonts/ionicons.woff", {
 
 app.import({
   development: 'bower_components/d3/d3.js',
-  production:  'bower_components/d3/d3.min.js'
+  production: 'bower_components/d3/d3.min.js'
 });
+
+app.import("bower_components/highlightjs/styles/github.css");
 
 app.import({
   development: 'bower_components/cryptojslib/rollups/sha3.js',
-  production:  'bower_components/cryptojslib/rollups/sha3.js'
+  production: 'bower_components/cryptojslib/rollups/sha3.js'
 });
 
 app.import("bower_components/bootstrap-less/js/dropdown.js");
 
 var tree = app.toTree();
 
-if(isProduction){
+if (isProduction) {
   tree = uglifyJavaScript(tree, {
     mangle: true,
     compress: {
-      sequences     : true,  // join consecutive statemets with the “comma operator”
-      properties    : true,  // optimize property access: a["foo"] → a.foo
-      dead_code     : true,  // discard unreachable code
-      drop_debugger : true,  // discard “debugger” statements
-      unsafe        : false, // some unsafe optimizations (see below)
-      conditionals  : true,  // optimize if-s and conditional expressions
-      comparisons   : true,  // optimize comparisons
-      evaluate      : true,  // evaluate constant expressions
-      booleans      : true,  // optimize boolean expressions
-      loops         : true,  // optimize loops
-      unused        : true,  // drop unused variables/functions
-      hoist_funs    : true,  // hoist function declarations
-      hoist_vars    : false, // hoist variable declarations
-      if_return     : true,  // optimize if-s followed by return/continue
-      join_vars     : true,  // join var declarations
-      cascade       : true,  // try to cascade `right` into `left` in sequences
-      side_effects  : true,  // drop side-effect-free statements
-      warnings      : true,  // warn about potentially dangerous optimizations/code
-      global_defs   : {}     // global definitions
+      sequences: true, // join consecutive statemets with the “comma operator”
+      properties: true, // optimize property access: a["foo"] → a.foo
+      dead_code: true, // discard unreachable code
+      drop_debugger: true, // discard “debugger” statements
+      unsafe: false, // some unsafe optimizations (see below)
+      conditionals: true, // optimize if-s and conditional expressions
+      comparisons: true, // optimize comparisons
+      evaluate: true, // evaluate constant expressions
+      booleans: true, // optimize boolean expressions
+      loops: true, // optimize loops
+      unused: true, // drop unused variables/functions
+      hoist_funs: true, // hoist function declarations
+      hoist_vars: false, // hoist variable declarations
+      if_return: true, // optimize if-s followed by return/continue
+      join_vars: true, // join var declarations
+      cascade: true, // try to cascade `right` into `left` in sequences
+      side_effects: true, // drop side-effect-free statements
+      warnings: true, // warn about potentially dangerous optimizations/code
+      global_defs: {} // global definitions
     }
   });
 }
--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api',
+  //host: 'http://selby.io'
 });
