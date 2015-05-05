85c5db48b5c49fbd0c4560cbf7ba0b9711f95793
selby <selby.kendrick@gmail.com>
2015-05-05 05:29:54 -0500

    perfect

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,8 +4,16 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, ResetScrollMixin, {
   actions: {
+    didTransition: function() {
+      if (this.controllerFor('application').get('currentPath').search('hn') !== -1) {
+        this.controllerFor('application').set('isNews', true);
+      } else {
+        this.controllerFor('application').set('isNews', false);
+      }
+      return true; // Bubble the didTransition event
+    },
     sessionAuthenticationSucceeded: function() {
-      //alert("omg");
+      //alert(this.get('currentPath'));
       this.transitionTo('posts');
     },
     sessionAuthenticationFailed: function(error) {
