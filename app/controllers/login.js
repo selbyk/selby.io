e37fd028054c854bf698a7219a2812faf101aba0
selby <selby.kendrick@gmail.com>
2015-05-17 17:11:58 -0500

    fix login front

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,9 +2,9 @@ import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
   authenticator: 'simple-auth-authenticator:oauth2-password-grant',
-  password: function(){
+  setPassword: function(){
     var hash = CryptoJS.SHA3(this.get('passwordInput'), { outputLength: 256 });
     //alert(hash.toString(CryptoJS.enc.Base64));
-    return hash.toString(CryptoJS.enc.Base64);
-  }.property('passwordInput'),
+    this.set('password', hash.toString(CryptoJS.enc.Base64));
+  }.observes('passwordInput')
 });
---
+++ app/routes/login.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import ResetScrollMixin from "selby.io/mixins/reset-scroll";
+
+export default Ember.Route.extend({ResetScrollMixin});
