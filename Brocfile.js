dc4ec19cf4089616e706a372bda97e28062567e2
selby <selby.kendrick@gmail.com>
2015-05-19 17:39:38 -0500

    oh god

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,7 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
-  isProduction = EmberApp.env() === 'production',
-  uglifyJavaScript = require('broccoli-uglify-js');
+  isProduction = EmberApp.env() === 'production';
 
 var app = new EmberApp({
   vendorFiles: {
@@ -72,35 +71,6 @@ app.import("bower_components/bootstrap-less/js/dropdown.js");
 
 app.import("bower_components/pdfjs-dist/build/pdf.combined.js");
 
-
-
 var tree = app.toTree();
-
-if (isProduction) {
-  tree = uglifyJavaScript(tree, {
-    mangle: true,
-    compress: {
-      sequences: true, // join consecutive statemets with the “comma operator”
-      properties: true, // optimize property access: a["foo"] → a.foo
-      dead_code: true, // discard unreachable code
-      drop_debugger: true, // discard “debugger” statements
-      unsafe: false, // some unsafe optimizations (see below)
-      conditionals: true, // optimize if-s and conditional expressions
-      comparisons: true, // optimize comparisons
-      evaluate: true, // evaluate constant expressions
-      booleans: true, // optimize boolean expressions
-      loops: true, // optimize loops
-      unused: true, // drop unused variables/functions
-      hoist_funs: true, // hoist function declarations
-      hoist_vars: false, // hoist variable declarations
-      if_return: true, // optimize if-s followed by return/continue
-      join_vars: true, // join var declarations
-      cascade: true, // try to cascade `right` into `left` in sequences
-      side_effects: true, // drop side-effect-free statements
-      warnings: true, // warn about potentially dangerous optimizations/code
-      global_defs: {} // global definitions
-    }
-  });
-}
 
 module.exports = tree;
--- app/models/hn-item.js
+++ app/models/hn-item.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 var hnItem = DS.Model.extend({
@@ -12,31 +13,36 @@ var hnItem = DS.Model.extend({
   type: DS.attr('string'), //The type of item. One of "job", "story", "comment", "poll", or "pollopt".
   url: DS.attr('string'),
   time: DS.attr('number'),
-  domain: function() {
-    if (this.get('url')) {
-      try {
-        var url_parts = (new URL(this.get('url')).hostname).split('.');
-        return '%@.%@'.fmt(url_parts[url_parts.length - 2], url_parts[url_parts.length - 1]);
-      } catch (e) {}
-
+  domain: Ember.computed("firstName", "lastName", {
+    get: function() {
+      if (this.get('url')) {
+        try {
+          var url_parts = (new URL(this.get('url')).hostname).split('.');
+          return '%@.%@'.fmt(url_parts[url_parts.length - 2], url_parts[url_parts.length - 1]);
+        } catch (e) {}
+      }
     }
-
-  }.property('url'),
-  commentsUrl: function() {
-    return '//news.ycombinator.com/item?id=%@'.fmt(this.id);
-  }.property('id'),
-  authorUrl: function() {
-    return '//news.ycombinator.com/user?id=%@'.fmt(this.get('by'));
-  }.property('by'),
-  publishedDate: function() {
-    return moment.unix(this.get('time')).fromNow();
-  }.property('time'),
-  isUrl: function() {
-    return this.get('type') === 'story';
-  }.property('type'),
-  isJob: function() {
-    return this.get('type') === 'job';
-  }.property('type')
+  }).property('url'),
+  commentsUrl: Ember.computed("firstName", "lastName", {
+    get: function() {
+      return '//news.ycombinator.com/user?id=%@'.fmt(this.get('by'));
+    }
+  }).property('by'),
+  publishedDate: Ember.computed("firstName", "lastName", {
+    get: function() {
+      return moment.unix(this.get('time')).fromNow();
+    }
+  }).property('time'),
+  isUrl: Ember.computed("firstName", "lastName", {
+    get: function() {
+      return this.get('type') === 'story';
+    }
+  }).property('type'),
+  isJob: Ember.computed("firstName", "lastName", {
+    get: function() {
+      return this.get('type') === 'job';
+    }
+  }).property('type')
 });
 
 export default hnItem;
