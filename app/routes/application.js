64353f081f14227851b608cbb6bae5b8ba58b132
selby <selby.kendrick@gmail.com>
2015-05-21 18:57:51 -0500

    fix code highlighting, build script

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,16 +5,11 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 export default Ember.Route.extend(ApplicationRouteMixin, ResetScrollMixin, {
   actions: {
     didTransition: function() {
-      var path = this.controllerFor('application').get('currentPath') || null;
-      if (path && path.search('hn') !== -1) {
-        this.controllerFor('application').set('isNews', true);
-      } else {
-        this.controllerFor('application').set('isNews', false);
-      }
-      return true; // Bubble the didTransition event
+      Ember.run.schedule('afterRender', this, this.checkIfNews);
+      Ember.run.schedule('afterRender', this, this.highlightCode);
+      return true;
     },
     sessionAuthenticationSucceeded: function() {
-      //alert(this.get('currentPath'));
       this.transitionTo('posts');
     },
     sessionAuthenticationFailed: function(error) {
@@ -27,5 +22,34 @@ export default Ember.Route.extend(ApplicationRouteMixin, ResetScrollMixin, {
         this.transitionTo('hn.index', q);
       }
     }
+  },
+  checkIfNews: function() {
+    var path = this.controllerFor('application').get('currentPath') || null;
+    if (path && path.search('hn') !== -1) {
+      this.controllerFor('application').set('isNews', true);
+    } else {
+      this.controllerFor('application').set('isNews', false);
+    }
+  },
+  highlightCode: function() {
+    Ember.$.ajaxSetup({
+      cache: true,
+      async: true
+    });
+    Ember.$.getScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js', function(data, textStatus) {
+      Ember.$('pre code').each(function(index, block) {
+        hljs.highlightBlock(block);
+      });
+      Ember.$('pre code').each(function(index, block) {
+        var pre = Ember.$(this).parent();
+        pre.addClass('hljs');
+        pre.html('<span class="line-number"></span>' + pre.html() + '<span class="cl"></span>');
+          var num = pre.html().split(/\n/).length;
+          for (var j = 0; j < num; j++) {
+            var line_num = pre.find('.line-number');
+            line_num.append('<span>' + (j + 1) + '</span>');
+          }
+      });
+    });
   }
 });
--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-ember build --environment=production --output-path=/usr/share/nginx/selby.io
+# Make tmp dir
+mkdir ./dist-prod
+# Build ember
+ember build --environment=production --output-path=./dist-prod
+# Remove old dist
+rm -r /usr/share/nginx/selby.io/*
+# Copy new build
+cp -r ./dist-prod/* /usr/share/nginx/selby.io/
+# Remove tmp dir
+rm -r ./dist-prod
